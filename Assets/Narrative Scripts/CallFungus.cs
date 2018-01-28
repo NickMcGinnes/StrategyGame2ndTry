69fc6548e7010852d7c25ede3e01ada91419c6e1
Unknown <c15526297@mydit.ie>
2018-01-28 17:39:14 +0000

    Fungus and Narative tests in

---
+++ Assets/Narrative Scripts/CallFungus.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class CallFungus : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Q)) {
+
+
+			Fungus.Flowchart.BroadcastFungusMessage ("Med");
+
+		}
+
+		if (Input.GetKeyDown (KeyCode.E)) {
+
+
+			Fungus.Flowchart.BroadcastFungusMessage ("Civ");
+
+		}
+
+		if (Input.GetKeyDown (KeyCode.W)) {
+
+			Fungus.Flowchart.BroadcastFungusMessage ("Supp");
+
+		}
+
+		if (Input.GetKeyDown (KeyCode.R)) {
+
+
+			Fungus.Flowchart.BroadcastFungusMessage ("Fight");
+
+		}
+	}
+}
