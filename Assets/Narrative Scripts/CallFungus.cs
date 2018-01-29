2d5854d09588a2503f77c5ea95e734768d766741
Unknown <c15526297@mydit.ie>
2018-01-29 18:21:11 +0000

    Prep for Event systems in Fungus

--- Assets/Narrative Scripts/CallFungus.cs
+++ Assets/Narrative Scripts/CallFungus.cs
@@ -10,13 +10,31 @@ public class CallFungus : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+		//Medical Ships Triggers
 		if (Input.GetKeyDown (KeyCode.Q)) {
 
-
+			//Med Distress
 			Fungus.Flowchart.BroadcastFungusMessage ("Med");
 
 		}
 
+		if (Input.GetKeyDown (KeyCode.A)) {
+
+			//Med Destroyed
+			Fungus.Flowchart.BroadcastFungusMessage ("MedDed");
+
+		}
+
+		if (Input.GetKeyDown (KeyCode.S)) {
+
+			//Med Survives
+			Fungus.Flowchart.BroadcastFungusMessage ("MedSuv");
+
+		}
+
+
+		//Civilian Ship Triggers
 		if (Input.GetKeyDown (KeyCode.E)) {
 
 
@@ -24,17 +42,39 @@ public class CallFungus : MonoBehaviour {
 
 		}
 
+
+		//Supply Ship Triggers
 		if (Input.GetKeyDown (KeyCode.W)) {
 
 			Fungus.Flowchart.BroadcastFungusMessage ("Supp");
 
 		}
 
+		if (Input.GetKeyDown (KeyCode.F)) {
+			//Supplies Destroyed
+			Fungus.Flowchart.BroadcastFungusMessage ("SupDed"); 
+			//Supplies Escaped
+			Fungus.Flowchart.BroadcastFungusMessage ("SupSuv");
+
+		}
+
+
+
+
+
+		//Fighter Ship Triggers
 		if (Input.GetKeyDown (KeyCode.R)) {
 
 
 			Fungus.Flowchart.BroadcastFungusMessage ("Fight");
 
+		}
+
+
+		if (Input.GetKeyDown(KeyCode.H))
+		{
+
+			Fungus.Flowchart.BroadcastFungusMessage("FightDed");
 		}
 	}
 }
